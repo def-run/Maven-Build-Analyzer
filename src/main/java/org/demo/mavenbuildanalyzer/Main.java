e2fbe118c3ff7087fd3ff3f31b35b761593585f2
Def <def17062026@gmail.com>
2026-08-09 11:06:22 +0530

    tested different log files

--- src/main/java/org/demo/mavenbuildanalyzer/Main.java
+++ src/main/java/org/demo/mavenbuildanalyzer/Main.java
@@ -1,6 +1,7 @@
 package org.demo.mavenbuildanalyzer;
 
 import org.demo.mavenbuildanalyzer.model.BuildAnalysis;
+import org.demo.mavenbuildanalyzer.model.ExceptionInfo;
 import org.demo.mavenbuildanalyzer.parser.MavenLogParser;
 
 import java.io.IOException;
@@ -11,14 +12,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         MavenLogParser parser = new MavenLogParser();
 
-        String log = Files.readString(Path.of("src/samples/maven-success.log"));
+        String log = Files.readString(Path.of("src/samples/maven-exception.log"));
 
         BuildAnalysis analysis = parser.parse(log);
 
-        System.out.println(analysis.getBuildStatus());
-        System.out.println(analysis.getBuildTime());
-        System.out.println(analysis.getFinishedAt());
-        System.out.println(analysis.getFailureType());
-        System.out.println();
+        System.out.println("Build Status: " + analysis.getBuildStatus());
+        System.out.println("Build Time: " + analysis.getBuildTime());
+        System.out.println("Finished At: " + analysis.getFinishedAt());
+        System.out.println("Failure Type: " + analysis.getFailureType());
+        System.out.println("Plugin executing when build failed: " + analysis.getPlugin());
+        System.out.println("Exceptions: ");
+        for(ExceptionInfo exceptions : analysis.getExceptions()) {
+            System.out.println(exceptions);
+        }
     }
 }
