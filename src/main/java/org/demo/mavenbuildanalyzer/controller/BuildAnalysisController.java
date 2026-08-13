cbb2891f408d1953bc65bb2f22e4696b5f084287
Def <def17062026@gmail.com>
2026-08-13 23:46:24 +0530

    created the controller class for build analysis

--- src/main/java/org/demo/mavenbuildanalyzer/controller/BuildAnalysisController.java
+++ src/main/java/org/demo/mavenbuildanalyzer/controller/BuildAnalysisController.java
@@ -1,4 +1,35 @@
 package org.demo.mavenbuildanalyzer.controller;
 
+import org.demo.mavenbuildanalyzer.model.BuildAnalysis;
+import org.demo.mavenbuildanalyzer.service.BuildAnalysisService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@RestController
+@RequestMapping("/api/builds")
 public class BuildAnalysisController {
+
+    private final BuildAnalysisService buildAnalysisService;
+
+    public BuildAnalysisController(BuildAnalysisService buildAnalysisService) {
+        this.buildAnalysisService = buildAnalysisService;
+    }
+
+    @PostMapping("/analyze")
+    public BuildAnalysis analyze(@RequestParam("file")MultipartFile file)
+            throws IOException {
+        if(file.isEmpty()) {
+            throw new IllegalArgumentException("File is empty");
+        }
+        String logContent = new String(file.getBytes(), StandardCharsets.UTF_8);
+
+        return buildAnalysisService.analyze(logContent);
+    }
 }
