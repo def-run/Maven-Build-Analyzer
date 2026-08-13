2c9f92c5411e664ccd30abba27a43fb530694505
Def <def17062026@gmail.com>
2026-08-13 23:48:46 +0530

    created global exception handler for handling bad requests

--- src/main/java/org/demo/mavenbuildanalyzer/exception/GlobalExceptionHandler.java
+++ src/main/java/org/demo/mavenbuildanalyzer/exception/GlobalExceptionHandler.java
@@ -1,4 +1,21 @@
 package org.demo.mavenbuildanalyzer.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.IOException;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<String> handleIOException(IOException e) {
+        return ResponseEntity.badRequest().body("Unable to read file");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
 }
