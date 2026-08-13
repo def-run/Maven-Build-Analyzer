798055929fcc75fda2b4e82935985766949e128a
Def <def17062026@gmail.com>
2026-08-13 23:47:59 +0530

    created build exception class

--- src/main/java/org/demo/mavenbuildanalyzer/entity/BuildException.java
+++ src/main/java/org/demo/mavenbuildanalyzer/entity/BuildException.java
@@ -1,4 +1,51 @@
 package org.demo.mavenbuildanalyzer.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "build_exceptions")
 public class BuildException {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String message;
+
+    @ManyToOne
+    @JoinColumn(name = "build_id")
+    private Build build;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Build getBuild() {
+        return build;
+    }
+
+    public void setBuild(Build build) {
+        this.build = build;
+    }
 }
